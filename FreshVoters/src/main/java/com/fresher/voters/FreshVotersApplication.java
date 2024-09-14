0ca62b6990906eb35e1b5271a6a6dac503ceb9ef
Sanjog_Grg <Sanjog_Grg@192.168.1.104>
2024-09-15 00:18:12 +0100

    initial checkin of fresh votes

---
+++ FreshVoters/src/main/java/com/fresher/voters/FreshVotersApplication.java
@@ -0,0 +1,13 @@
+package com.fresher.voters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshVotersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshVotersApplication.class, args);
+	}
+
+}
---
+++ FreshVoters/src/test/java/com/fresher/voters/FreshVotersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fresher.voters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshVotersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
